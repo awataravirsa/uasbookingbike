7838a995d2747e8cf9d8695c254c59a493fb81e0
awataravirsa <ariawataravirsa@gmail.com>
2021-06-22 02:49:57 +0800

    project uas

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="jumbotorn text-center">
+
+        <div class="text-center">
+            <h3>Welcome to Booking Bicycle Pica</h3>
+            <h3>Have Fun Guysss</h3>
+
+        </div>
+
+</x-app-layout>
